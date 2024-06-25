127db51739a105f9e72f044b5bef1ee35252ffb6
debian <tl@zasmlabs.com>
2024-06-25 10:27:52 +0530

    tried  genui streaming response for the first time

---
+++ python-genui/frontend/utils/client.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode, useContext, createContext } from "react";
+
+const ActionsContext = createContext<any>(null);
+
+export const AIProvider = (props: {
+  actions: Record<string, any>;
+  children: ReactNode;
+}) => {
+  return (
+    <ActionsContext.Provider value={props.actions}>
+      {props.children}
+    </ActionsContext.Provider>
+  );
+};
+
+export function useActions<T extends {$$types?: Record<string, unknown>}>() {
+  return useContext(ActionsContext) as Exclude<T["$$types"],undefined>;
+}
